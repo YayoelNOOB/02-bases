74d9ad5de86aedebd8f49986d1616f3b94d461a4
Rafael Alejandro Domínguez Junco <yayo281001@hotmail.com>
2024-03-08 09:36:28 -0600

    08/03

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/characters.interface';
 
 @Component({
   selector: 'dbz-add-character',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './add-character.component.css'
 })
 export class AddCharacterComponent {
+
+  // El eventEmitter nos ayuda a crear una instancia de emisor de eventos. Se tiene que decorar con @Output()
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
+  public character: Character = {
+    name:'',
+    power: 0
+  };
+
+  emitCharacter(): void{
+    console.log(this.character);
+    // El if es para que nadie pueda enviar un personaje sin nombre
+    if( this.character.name.length === 0 ) return;
+    //Si tiene nombre hay que emitir el personaje con la siguiente línea
+    this.onNewCharacter.emit(this.character);
+
+    this.character.name = '';
+    this.character.power = 0
+  }
 
 }
